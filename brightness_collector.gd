25c66680f882695438745d2dc53801a9a517dc04
Jannik Schäfer <me@jschaefer.io>
2022-05-22 02:52:54 +0200

    Bugfix: fix bug preventing proper aggregation of brightness values

--- brightness_collector.gd
+++ brightness_collector.gd
@@ -20,13 +20,15 @@ func _get_configuration_warning():
 	return ""
 	
 func collect() -> float:
-	var check: float
-	var count = 0
-	var light_level: float = 0.0
+	var collected_level: float = 0.0
+	var count: int
+	var level: float
 	for light in get_tree().get_nodes_in_group(collector_group):
+		count = 0.0
+		level = 0.0
 		for child in children:
-			light_level += child.influence * light.get_light_level(child.global_transform.origin, excludes, raycast_layers)
+			level += child.influence * light.get_light_level(child.global_transform.origin, excludes, raycast_layers)
 			count += child.influence
-	if count == 0:
-		return light_level
-	return light_level / count
+		if count != 0:
+			collected_level += level / count
+	return min(collected_level, 1)
